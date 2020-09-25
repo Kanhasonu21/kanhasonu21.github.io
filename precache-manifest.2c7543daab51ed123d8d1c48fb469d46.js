37553a12e44fb6a02fff3e64c1b06ae26d5b4841
Kanhaiya.Kumar <kanhaiya.kumar@webdunia.net>
2020-09-25 13:18:34 +0530

    second commit

---
+++ precache-manifest.2c7543daab51ed123d8d1c48fb469d46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13ce218c74002fc4e7a593c4c87703c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a465f7e06d2df3f98265",
+    "url": "/static/css/main.538e0fb0.chunk.css"
+  },
+  {
+    "revision": "df2b10dc2d61d6b7151c",
+    "url": "/static/js/2.366ff5a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.366ff5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a465f7e06d2df3f98265",
+    "url": "/static/js/main.47ba683b.chunk.js"
+  },
+  {
+    "revision": "263349ffc2876e0ee9cc",
+    "url": "/static/js/runtime-main.81d36724.js"
+  },
+  {
+    "revision": "2560fe532cad3fea62e4290e8d357219",
+    "url": "/static/media/578946_stormy-weather-wallpapers-hd-wallpaper-backgrounds-of-your-choice_1989x1080_h.2560fe53.jpg"
+  }
+]);
